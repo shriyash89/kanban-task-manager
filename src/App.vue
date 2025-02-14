cf82fae776440d7db4502672571c9c8fd1d47553
shriyash89 <shriyansh.r@incquet.com>
2025-02-14 18:38:20 +0530

    pinia added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,27 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>count is {{ count }}</p>
+    <p>double of it is {{ double }}</p>
+    <button @click="increment()">inc</button>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'pinia';
 import HelloWorld from './components/HelloWorld.vue'
+import { useCounterStore } from './store';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  computed : {
+    ...mapState(useCounterStore, ['count','double'])
+  },
+  methods : {
+    ...mapActions(useCounterStore, ['increment'])
   }
 }
 </script>
